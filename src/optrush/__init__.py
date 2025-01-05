8c407a266e81bfb35bb19e8b6d7b53ffdfee495f
chenxu <chen.xu.wq@gmail.com>
2025-01-05 16:47:36 +0800

    init

---
+++ src/optrush/__init__.py
@@ -0,0 +1,42 @@
+from optrush._core import norm_cdf
+from optrush._core import norm_pdf
+from optrush._core import bs_d1
+from optrush._core import bs_d2
+from optrush._core import bs_call_price
+from optrush._core import bs_put_price
+from optrush._core import bs_call_delta
+from optrush._core import bs_put_delta
+from optrush._core import bs_gamma
+from optrush._core import bs_vega
+from optrush._core import bs_call_theta
+from optrush._core import bs_put_theta
+from optrush._core import bs_call_rho
+from optrush._core import bs_put_rho
+from optrush._core import bk_d1
+from optrush._core import bk_d2
+from optrush._core import bk_call_price
+from optrush._core import bk_put_price
+from optrush._core import bk_call_delta
+from optrush._core import bk_put_delta
+from optrush._core import bk_gamma
+from optrush._core import bk_vega
+from optrush._core import bk_call_theta
+from optrush._core import bk_put_theta
+from optrush._core import bk_call_rho
+from optrush._core import bk_put_rho
+from optrush._core import bach_d
+from optrush._core import bach_call_price
+from optrush._core import bach_put_price
+from optrush._core import bach_call_delta
+from optrush._core import bach_put_delta
+from optrush._core import bach_gamma
+from optrush._core import bach_vega
+from optrush._core import bach_call_theta
+from optrush._core import bach_put_theta
+from optrush._core import bach_call_rho
+from optrush._core import bach_put_rho
+from optrush._core import implied_volatility
+
+
+# def main() -> None:
+#    print(hello_from_bin())
